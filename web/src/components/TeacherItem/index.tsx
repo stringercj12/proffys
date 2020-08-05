21efd077fa459027946efb70d09e1a187736231b
Jefferson Ferreira <jefferson14489@gmail.com>
2020-08-05 20:44:45 -0300

    Semana nlw rockeseat - aplicação web

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+const TeacherItem: React.FC = () => {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars3.githubusercontent.com/u/43593688?s=460&u=5f5f0340b81e51f93dc85591b7a6568e47f59189&v=4" alt="Jefferson Ferreira" />
+                <div>
+                    <strong>Jefferson Ferreira</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+            <p>
+                Entusiasta das melhores tecnologias de química avançada.
+                <br />
+                <br />
+                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+            </p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                            <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                            Entrar em contato
+                        </button>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem;
